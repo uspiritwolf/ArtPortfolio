9f9c9a5935749783cfcd361d8e626eea775da609
Oleksii Chernykh <uspiritwolf@outlook.com>
2023-05-28 01:01:21 +0300

    Small tweaks in the authors section

--- src/index.js
+++ src/index.js
@@ -11,11 +11,11 @@ function Overlay() {
   const snap = useSnapshot(state)
   return (
     <div style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', pointerEvents: 'none' }}>
-      <a href="https://github.com/uspiritwolf" style={{ position: 'absolute', bottom: 40, left: 90, fontSize: '17px' }}>
-        Dev: uspriitwolf
-      </a>
-      <a href="https://www.artstation.com/riiakarpenkoo" style={{ position: 'absolute', bottom: 60, left: 90, fontSize: '17px' }}>
+      <a href="https://www.artstation.com/riiakarpenkoo" style={{ position: 'absolute', bottom: 65, left: 90, fontSize: '17px' }}>
         Art: riiakarpenkoo
+      </a>
+      <a href="https://github.com/uspiritwolf" style={{ position: 'absolute', bottom: 35, left: 90, fontSize: '17px' }}>
+        Dev: Oleksii Chernykh
       </a>
       <div className="shadowText" style={{ position: 'absolute', bottom: 40, right: 40, fontSize: '17px' }}>05/27/2023</div>
       { snap.currentPage > 0 && 
