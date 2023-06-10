3bb17943c06d65f2ba976ce3faadc582fa38deb2
Oleksii Chernykh <7448849+uspiritwolf@users.noreply.github.com>
2023-06-10 20:18:59 +0300

    Add a lego model created by riiakarpenkoo

--- src/State.js
+++ src/State.js
@@ -3,9 +3,10 @@ import { proxy } from 'valtio'
 import React from 'react'
 import { Page1 } from './Models/Page1'
 import { Page2 } from './Models/Page2'
+import { Page3 } from './Models/Page3'
 
 const state = proxy({
-	pages: [<Page1/>, <Page2/>],
+	pages: [<Page1/>, <Page2/>, <Page3/>],
 	currentPage: 0
 })
 
